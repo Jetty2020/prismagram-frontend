da6921f92f9f2d853387a0f27915559e1a1132b5
Jetty2020 <kmclo@naver.com>
2020-02-08 01:29:54 +0900

    #7.3 Post Component part Three

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
  import PropTypes from "prop-types";
  import useInput from "../../Hooks/useInput";
  import PostPresenter from "./PostPresenter";
@@ -16,7 +16,18 @@ import React, { useState } from "react";
  }) => {
   const [isLikedS, setIsLiked] = useState(isLiked);
   const [likeCountS, setLikeCount] = useState(likeCount);
+  const [currentItem, setCurrentItem] = useState(0);
   const comment = useInput("");
+
+  useEffect(() => {
+    const totalFiles = files.length;
+    if (currentItem === totalFiles - 1) {
+      setTimeout(() => setCurrentItem(0), 3000);
+    } else {
+      setTimeout(() => setCurrentItem(currentItem + 1), 3000);
+    }
+    }, [currentItem, files]);
+
   return (
     <PostPresenter
       user={user}
@@ -30,6 +41,7 @@ import React, { useState } from "react";
       newComment={comment}
       setIsLiked={setIsLiked}
       setLikeCount={setLikeCount}
+      currentItem={currentItem}
     />
   );
  };

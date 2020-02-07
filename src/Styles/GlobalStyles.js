a784833f9f56b2eb6a5c752da64287cbdf32cafc
Jetty2020 <kmclo@naver.com>
2020-02-07 09:41:59 +0900

    #6.0 Header UI

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+
 export default createGlobalStyle`
     ${reset};
     @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
@@ -11,6 +12,7 @@ export default createGlobalStyle`
         color:${props => props.theme.blackColor};
         font-size:14px;
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        padding-top: 140px;
     }
     a {
          color:${props => props.theme.blueColor};
